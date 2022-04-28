0ca49dd4998790c9f1ce297498c4698c63d1d73d
davidwarshawsky <davidawarshawsky@gmail.com>
2022-04-28 18:14:26 -0400

    Updated version with data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React,{ useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { TextField, Button } from '@mui/material';
+// import { rootShouldForwardProp } from '@mui/material/styles/styled';
+
+
+function ClassSearch(){
+  const [subject, setSubject] = useState("");
+  const [number, setNumber] = useState("");
+
+  function handleSubjectChange(event){
+    setSubject(event.target.value);
+    console.log(subject);
+  }
+  function handleNumberChange(event){
+    setNumber(event.target.value);
+    console.log(number);
+  }
+  function handleClick(){
+    console.log(subject + " " + number);
+    getCoursesFromApi();
+  }
+  async function getCoursesFromApi() {
+    console.log(subject + " " + number);
+  try {
+      const url = 'http://127.0.0.1:5000/' + subject + "/" + number + "/";
+      let response = await fetch(url);
+      let responseJson = await response.json();
+      console.log(typeof(responseJson));
+      return console.table(responseJson);
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+  return(
+    // https://www.pluralsight.com/guides/fetch-data-from-a-json-file-in-a-react-app
+    <div>
+      <h1>SJSU Transferable Classes Search</h1>
+      <TextField 
+        value = {subject}
+        onChange={handleSubjectChange}
+        type = "search"
+        id="outlined-basic"
+        variant="outlined"
+      />
+      <TextField 
+        value = {number}
+        onChange={handleNumberChange} 
+        type = "search"
+        id="outlined-basic"
+        variant="outlined"        
+      />
+      <Button variant="contained" onClick = {handleClick}>SUBMIT</Button>
+    </div>
+  );
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ClassSearch />
   </React.StrictMode>,
   document.getElementById('root')
 );
