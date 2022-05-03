66b2c11dd9deff1fbc1517c87c23e9fba655f784
davidwarshawsky <davidawarshawsky@gmail.com>
2022-05-03 12:08:09 -0400

    Got table of data to show up on search

--- src/index.js
+++ src/index.js
@@ -11,50 +11,90 @@ function ClassSearch(){
   const [subject, setSubject] = useState("");
   const [number, setNumber] = useState("");
 
+  const [responseJson, setResponseJson] = useState(Object());
+  const [jsonRender, setJsonRender] = useState(Object());
+// Done
   function handleSubjectChange(event){
     setSubject(event.target.value);
     console.log(subject);
   }
+// Done
   function handleNumberChange(event){
     setNumber(event.target.value);
     console.log(number);
   }
+
+  // Done
   function handleClick(){
     console.log(subject + " " + number);
     getCoursesFromApi();
   }
+
+  // Todo 
   async function getCoursesFromApi() {
     console.log(subject + " " + number);
   try {
+      // Gets the data and sets responseJson
       const url = 'http://127.0.0.1:5000/' + subject + "/" + number + "/";
       let response = await fetch(url);
-      let responseJson = await response.json();
-      console.log(typeof(responseJson));
-      return console.table(responseJson);
+      let responseJsonified = await response.json();
+      setResponseJson(responseJsonified);
+      setJsonRender(returnData(responseJson));
     } catch(error) {
       console.error(error);
     }
   }
 
+  // https://stackoverflow.com/questions/22876978/loop-inside-react-jsx?page=1&tab=createdasc#tab-top
+  function returnData(jsonData) {
+    var rows = [];
+    for (var i = 0; i < Object.keys(jsonData).length; i++) {
+      rows.push(
+        <tr>
+          <td>{jsonData[i][0]}</td>
+          <td>{jsonData[i][1]}</td>
+        </tr>
+      );
+    }
+    return rows;
+  }
+
   return(
     // https://www.pluralsight.com/guides/fetch-data-from-a-json-file-in-a-react-app
     <div>
-      <h1>SJSU Transferable Classes Search</h1>
-      <TextField 
-        value = {subject}
-        onChange={handleSubjectChange}
-        type = "search"
-        id="outlined-basic"
-        variant="outlined"
-      />
-      <TextField 
-        value = {number}
-        onChange={handleNumberChange} 
-        type = "search"
-        id="outlined-basic"
-        variant="outlined"        
-      />
-      <Button variant="contained" onClick = {handleClick}>SUBMIT</Button>
+      <div>
+        <h1>SJSU Transferable Classes Search</h1>
+        <TextField 
+          value = {subject}
+          onChange={handleSubjectChange}
+          type = "search"
+          id="outlined-basic"
+          variant="outlined"
+        />
+        <TextField 
+          value = {number}
+          onChange={handleNumberChange} 
+          type = "search"
+          id="outlined-basic"
+          variant="outlined"        
+        />
+        <Button variant="contained" onClick = {handleClick}>SUBMIT</Button>
+      </div>
+      {Object.keys(jsonRender).length > 0 &&
+      <div>
+       <table>
+         <thead>
+           <tr>
+             <th>College</th>
+             <th>Equivalent Course</th>
+           </tr>
+         </thead>
+         <tbody>
+           {jsonRender}
+         </tbody>
+        </table>
+      </div>
+      }
     </div>
   );
 }
