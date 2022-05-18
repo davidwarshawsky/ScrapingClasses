fac8c751f28ec6aa7a05be4bf5461dbec6a8fd97
davidwarshawsky <davidawarshawsky@gmail.com>
2022-05-18 17:22:21 -0400

    Got the website how I like it

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React,{ useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
@@ -9,126 +9,7 @@ import { TextField, Button } from '@mui/material';
 
 
 
-function ClassSearchFunction(){
-  // Stores subject and number
-  const [subject, setSubject] = useState("");
-  const [number, setNumber] = useState("");
 
-  // Stores JSON response from Server
-  const [responseJson, setResponseJson] = useState(Object([]));
-  const [jsonRender, setJsonRender] = useState(Object([]));
-
-  // Done
-  function handleSubjectChange(event){
-    setSubject(event.target.value);
-    // console.log(subject);
-  }
-// Done
-  function handleNumberChange(event){
-    setNumber(event.target.value);
-    // console.log(number);
-  }
-
-  // Done
-  function handleClick(){
-    console.log("BUTTON CLICKED");
-    console.log("Search is for: " + subject + " " + number);
-    getCoursesFromApi();
-    console.log("Subject type: " + typeof(subject));
-    console.log("Number type: " + typeof(number));
-    console.log(responseJson);
-    console.log("Type of responseJson: " + typeof(responseJson));
-    console.log("Type of jsonRender: " + typeof(jsonRender));
-    console.log(jsonRender);
-    console.log("jsonRender length: " + jsonRender.length);
-    setSubject(subject);
-    }
-
-  // Todo 
-  async function getCoursesFromApi() {
-  try {
-      // Gets the data and sets responseJson
-      const url = 'http://127.0.0.1:5000/' + subject + "/" + number + "/";
-      // console.log("getCoursesFromApi url: " + url);
-      let response = await fetch(url);
-      // console.log("response");
-      // console.log(response);
-      let responseJsonified = await response.json();
-      // console.log("responseJsonified: ");
-      // console.log(responseJsonified);
-      // console.log(typeof(responseJsonified));
-      setResponseJson(responseJsonified);
-      // console.log("responseJson in getCoursesFromApi");
-      // console.log(responseJson);
-      // For debugging added logging.
-      // console.log("responseJson length: " + String(Object.keys(responseJson).length));
-      const ren = returnData(responseJson);
-      setJsonRender(ren);
-      // setJsonRender(returnData(responseJson));
-      // console.log("jsonRender length: " + String(Object.keys(jsonRender).length));
-      // setRerender(rerender === 0 ? 1: 0);
-      // console.log("Rerender value: " + String(rerender));
-    } catch(error) {
-      console.error(error);
-    }
-  }
-
-  // https://stackoverflow.com/questions/22876978/loop-inside-react-jsx?page=1&tab=createdasc#tab-top
-  function returnData(jsonData) {
-    var rows = [];
-    console.log("jsonData length: " + Object.keys(jsonData).length);
-    for (var i = 0; i < Object.keys(jsonData).length; i++) {
-      console.log(jsonData[i][0]);
-      console.log(jsonData[i][1]);
-      rows.push(
-        <tr key={i}>
-          <td>{jsonData[i][0]}</td>
-          <td>{jsonData[i][1]}</td>
-        </tr>
-      );
-    }
-    return rows;
-  }
-  console.log("SUBJECT: " + subject + "\n" + "NUMBER: " + number);
-  return(
-    // https://www.pluralsight.com/guides/fetch-data-from-a-json-file-in-a-react-app
-    <div>
-      <div className='form'>
-        <h1>SJSU Transferable Classes Search</h1>
-        <TextField 
-          value = {subject}
-          onChange={handleSubjectChange}
-          type = "search"
-          id="outlined-basic"
-          variant="outlined"
-        />
-        <TextField 
-          value = {number}
-          onChange={handleNumberChange} 
-          type = "search"
-          id="outlined-basic"
-          variant="outlined"        
-        />
-        <Button variant="contained" onClick = {handleClick}>SUBMIT</Button>
-      </div>
-      {Object.keys(jsonRender).length > 0 &&
-      <div>
-       <table>
-         <thead>
-           <tr>
-             <th>College</th>
-             <th>Equivalent Course</th>
-           </tr>
-         </thead>
-         <tbody>
-           {jsonRender}
-         </tbody>
-        </table>
-      </div>
-      }
-    </div>
-  );
-}
 
 class ClassSearch extends React.Component { 
   constructor(props){
@@ -145,6 +26,8 @@ class ClassSearch extends React.Component {
       clicked: false,
       responseJson: [],
       rows: [],
+      // valid: false,
+      
     }
   }
 
@@ -158,8 +41,10 @@ class ClassSearch extends React.Component {
         console.log(this.state.responseJson);
         // console.log(typeof(this.state.responseJson));
         this.returnRows(this.state.responseJson);
+        this.setState({valid:true});
     }catch(error) {
       console.error(error);
+      this.setState({valid:false});
     }
   }
 
@@ -205,6 +90,8 @@ class ClassSearch extends React.Component {
       <div>
         <div className='form'>
           <h1>SJSU Transferable Classes Search</h1>
+          <div>
+          <h2>Subject</h2>
           <TextField 
             value = {this.state.subject}
             onChange={this.handleSubjectChange}
@@ -212,6 +99,9 @@ class ClassSearch extends React.Component {
             id="outlined-basic"
             variant="outlined"
           />
+          </div>
+          <div>
+          <h2>Number</h2>
           <TextField 
             value = {this.state.number}
             onChange={this.handleNumberChange} 
@@ -219,8 +109,14 @@ class ClassSearch extends React.Component {
             id="outlined-basic"
             variant="outlined"        
           />
+          </div>
           <Button variant="contained" onClick = {this.handleClick}>SUBMIT</Button>
-          {this.state.clicked && <Button>YOU CLICKED THE BUTTON!</Button>}
+          {/* valid input
+              invalid input
+              SHOWS UP AS INVALID UNTIL I  
+              */}
+          {/* {!this.state.valid && this.state.clicked && <p>{this.state.subject} {this.state.number} is invalid</p>} */}
+          <p>Please enter the subject abbreviation and class number exactly. Example: BIOL 0650 , ENGL 001B</p>
         </div>
         <div>
         <table>
