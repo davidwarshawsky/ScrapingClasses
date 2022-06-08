037b64d5b0890f63be6f8a9700aee7129e512163
davidwarshawsky <davidawarshawsky@gmail.com>
2022-06-08 14:28:19 -0400

    Fucked with the css to try and get the h2 and select elements side by side like in the BMI calculator of my SVCTE  class

--- src/index.js
+++ src/index.js
@@ -49,8 +49,8 @@ class ClassSearch extends React.Component {
 
   async getCourseFromApi() {
     try {
-      // const url = 'http://172.17.0.2:8080/' + this.state.subjectClicked + "/" + this.state.numberClicked + "/";
-      const url = 'https://flask-scraping-classes-api.b6eftcit5eg44.us-west-2.cs.amazonlightsail.com/' + this.state.subjectClicked + "/" + this.state.numberClicked + "/";
+      const url = 'http://127.0.0.1:8080/' + this.state.subjectClicked + "/" + this.state.numberClicked + "/";
+      // const url = 'https://flask-scraping-classes-api.b6eftcit5eg44.us-west-2.cs.amazonlightsail.com/' + this.state.subjectClicked + "/" + this.state.numberClicked + "/";
       let response = await fetch(url);
       let responseJsonified = await response.json();
       this.setState({responseJson: responseJsonified});
@@ -111,27 +111,34 @@ class ClassSearch extends React.Component {
     // console.log(this.state.rows.length);
     return (
       <html>
-      <head>
-        <title>SJSU Transfer</title>
-      </head>
-      <div>
-        <h1>SJSU Transferable Classes Search</h1>
-        <div className='form'>
-          <div className='selector'>
-            <div>
+        <head>
+          <title>SJSU Transfer</title>
+        </head>
+        <div>
+          <div>
+            <h1>SJSU Transferable Classes Search</h1>
+          </div>
+          <div className='navigation'>
+          {/* <div className='selector'> */}
+            <div className='row'>
               <h2>Subject</h2>
-              <select id="subject" onClick={this.handleSubjectClicked} defaultValue={"CS"} value={this.state.subjectClicked}>
-                {Object.keys(valid_courses).map((key,i) => ( <option key = {i}>{key}</option>))}
+              <select 
+                id="subject" onClick={this.handleSubjectClicked} defaultValue={"CS"}
+                value={this.state.subjectClicked}>
+                {Object.keys(valid_courses).map((key,i) => ( <option key = {i}>{key}</option>
+                ))}
               </select>
             </div>
-            <div id="number">
+            <div className='row'>
               <h2>Number</h2>
-              <select id="number" onClick={this.handleNumberClicked} defaultValue="046A" value={this.state.numberClicked}>
+              <select 
+                id="number" onClick={this.handleNumberClicked} defaultValue="046A" value={this.state.numberClicked}>
                 {valid_courses[this.state.subjectClicked].map((number,i) => (<option key = {i}>{number}</option>))}
               </select>
             </div>
-            <Button variant="contained" onClick = {this.handleClick}>SUBMIT</Button>
           </div>
+          <div><Button variant="contained" onClick = {this.handleClick}>SUBMIT</Button></div>
+          {/* </div> */}
           {/* <TextField 
             value = {this.state.subject}
             onChange={this.handleSubjectChange}
@@ -146,28 +153,45 @@ class ClassSearch extends React.Component {
             id="outlined-basic"
             variant="outlined"        
           /> */}
-        </div>
           {/* valid input
               invalid input
               SHOWS UP AS INVALID UNTIL I  
               */}
           {/* {!this.state.valid && this.state.clicked && <p>{this.state.subject} {this.state.number} is invalid</p>} */}
           {/* <p>Please enter the subject abbreviation and class number exactly. Example: BIOL 0650 , ENGL 001B</p> */}
-        <div>
-        <div>
-        <table>
-          <thead>
-            <tr>
-              <th>College</th>
-              <th>Equivalent Course</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.rows}
-          </tbody>
-        </table>
-      </div>
-      </div>
+          <div className='casestudy'>
+            <table>
+              <thead>
+                <tr>
+                  <th>College</th>
+                  <th>Equivalent Course</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.rows}
+              </tbody>
+            </table>
+          </div>
+      {/* <div>
+        <p>
+          This website is meant for SJSU students who would like to take courses at 
+          nearby schools or community colleges for the winter,summer, or would like to
+          save money and fulfill some of their GE requirements by taking a semester off.
+          This will help find equivalent courses so that a combination of online and in-person
+          courses that transfer to SJSU can be taken.
+        </p>
+        <p>
+          use <a href="https://www.csufullyonline.com/">csufullyonline</a>
+          to find fully online courses. This website will help you find equivalent in-person
+          and online courses at nearby community colleges or universities to fullfill GE 
+          requirements at your 4 year university.
+        </p>
+        <p>
+          Use <a href="https://assist.org/">assist.org</a> to find equivalent 
+          transfer agreements by major between schools OR which courses at your
+          community college transfers to the UC or CSU system. 
+        </p>
+      </div> */}
       <footer>
         <div id="footer">
           <p >©2022 David Warshawsky</p>
@@ -181,8 +205,12 @@ class ClassSearch extends React.Component {
 }
 
 // ToDo list \
-// Make a dropdown bar for school, course, and if it transfers to SJSU, 
-// and equivalent course
+// Make a list of checkboxes with universities that a student will consider
+// Add a dropdown menu for the course the student wants to find equivalent at another school
+// if it exists.
+// There will be an add button so that way the student can see a list of the courses
+// in a table that will transfer over and what the equivalent course will be. 
+
 
 // class TransferableDropdown extends React.Component(){
 //   constructor(props){
